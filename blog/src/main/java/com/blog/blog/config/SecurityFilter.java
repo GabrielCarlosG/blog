7fce24a01fc13ddb388a3e7cfb92fed4e8a6f2e2
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-04-22 00:37:50 -0300

    atualizacao gitignore

--- blog/src/main/java/com/blog/blog/config/SecurityFilter.java
+++ blog/src/main/java/com/blog/blog/config/SecurityFilter.java
@@ -1,13 +1,5 @@
 package com.blog.blog.config;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.blog.blog.models.User;
 import com.blog.blog.repositories.UserRepository;
 import com.blog.blog.services.AuthenticationService;
@@ -16,7 +8,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter{
    
---
+++ blog/src/test/java/com/blog/blog/config/SecurityFilterTest.java
@@ -0,0 +1,10 @@
+package com.blog.blog.config;
+
+import org.junit.jupiter.api.Test;
+
+public class SecurityFilterTest {
+    @Test
+    void testDoFilterInternal() {
+
+    }
+}
