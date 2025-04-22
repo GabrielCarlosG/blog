f664331c1cbee463bc8f9ebdb5cb04587ebd2e24
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-04-22 00:17:03 -0300

    Security filter

--- blog/src/main/java/com/blog/blog/services/AuthenticationService.java
+++ blog/src/main/java/com/blog/blog/services/AuthenticationService.java
@@ -8,6 +8,8 @@ import com.blog.blog.request.AuthRequest;
 public interface AuthenticationService {
     UserDetails loadUserByUsername(String login) throws UsernameNotFoundException;
 
+    
     String getToken(AuthRequest auth);
 
+    String validateJwtToken(String token);
 }
