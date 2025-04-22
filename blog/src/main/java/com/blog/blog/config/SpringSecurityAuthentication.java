25fb333539513413762335abbb31d679d964c066
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-04-22 00:33:20 -0300

    Adicao em SpringSecurity, tem problema de compiler in SecurityFilter

--- blog/src/main/java/com/blog/blog/config/SpringSecurityAuthentication.java
+++ blog/src/main/java/com/blog/blog/config/SpringSecurityAuthentication.java
@@ -1,5 +1,13 @@
 package com.blog.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityAuthentication {
 
+    @Autowired
+    private SecurityFilter securityFilter;
 }
